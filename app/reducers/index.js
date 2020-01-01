cf4f41a5feb2f1ff437cfc88aeecc17ab7fcd9e0
Trey <trey@lavidadev.com>
2020-01-01 05:11:27 -0500

    Add search to reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import type { HashHistory } from 'history';
 import counter from './counter';
+import search from './search';
 
 export default function createRootReducer(history: HashHistory) {
   return combineReducers<{}, *>({
     router: connectRouter(history),
-    counter
+    counter,
+    search
   });
 }
