619cccde768f21d53ab015bf9bff40ec603743a4
Trey <trey@lavidadev.com>
2020-01-02 23:23:32 -0500

    Update export function

--- app/actions/search.js
+++ app/actions/search.js
@@ -1,9 +1,17 @@
 // @flow
 export const SET_SEARCH_PARAM = 'SET_SEARCH_PARAM';
+export const SET_RETURNED_SEARCH_DATA = 'SET_RETURNED_SEARCH_DATA';
 
 export function setSearchParam(payload: string) {
   return {
     type: SET_SEARCH_PARAM,
+    payload
+  };
+}
+
+export function setReturnedSearchData(payload: string) {
+  return {
+    type: SET_RETURNED_SEARCH_DATA,
     payload
   };
 }
