8fcffc1fe9be2b2479080166cbeddc2b40971f96
Trey <trey@lavidadev.com>
2020-01-02 23:25:08 -0500

    Add new reducers function

--- app/reducers/search.js
+++ app/reducers/search.js
@@ -1,10 +1,19 @@
 // @flow
-import { SET_SEARCH_PARAM } from '../actions/search';
-import type { Action } from './types';
+import { SET_SEARCH_PARAM, SET_RETURNED_SEARCH_DATA } from '../actions/search';
+import type { Action, State } from './types';
 
-export default function search(state: string = '', action: Action) {
+export function searchName(state: string = '', action: Action) {
   switch (action.type) {
     case SET_SEARCH_PARAM:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function searchData(state: State = [], action: Action) {
+  switch (action.type) {
+    case SET_RETURNED_SEARCH_DATA:
       return action.payload;
     default:
       return state;
