38fb7e3a86b78cd3d7510959673afd4e27a65392
Trey <trey@lavidadev.com>
2020-01-02 23:24:45 -0500

    Fixed reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,12 +3,13 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import type { HashHistory } from 'history';
 import counter from './counter';
-import search from './search';
+import { searchName, searchData } from './search';
 
 export default function createRootReducer(history: HashHistory) {
   return combineReducers<{}, *>({
     router: connectRouter(history),
     counter,
-    search
+    searchName,
+    searchData
   });
 }
