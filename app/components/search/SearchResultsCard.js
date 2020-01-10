30cb463c7dd01a43d0210cf5a884cdf2d02620ea
Trey <trey@lavidadev.com>
2020-01-09 21:14:57 -0500

    Fix for getting image height

--- app/components/search/SearchResultsCard.js
+++ app/components/search/SearchResultsCard.js
@@ -7,12 +7,37 @@ type Props = {
   dataName: string
 };
 
+type ReactObjRef<ElementType: React.ElementType> = {
+  current: null | React.ElementRef<ElementType>
+};
+
 export default class SearchResultsCard extends React.Component<Props> {
-  props: Props;
+  reference: ReactObjRef<'img'>;
+
+  constructor(props: Props) {
+    super(props);
+    this.reference = React.createRef();
+  }
+
+  componentDidMount() {
+    // $FlowFixMe
+    this.reference.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    // $FlowFixMe
+    console.log(this.reference.current.clientHeight);
+  };
 
   render() {
     const { dataUrl, dataName } = this.props;
-
-    return <img className={styles.posterImg} src={dataUrl} alt={dataName} />;
+    return (
+      <img
+        ref={this.reference}
+        className={styles.posterImg}
+        src={dataUrl}
+        alt={dataName}
+      />
+    );
   }
 }
